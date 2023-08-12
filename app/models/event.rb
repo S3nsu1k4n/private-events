ff3148d70aa6cacae770c457dae4bcb8ff54e80b
S3nsu1k4n <saschakoeppe@gmail.com>
2023-08-12 11:09:21 +0900

    add attendees relations and add its model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendees, source: :event_attendee
 
   validates :name, :location, :date, presence: true
   validates :name, :location, length: { in: 5..64 }
