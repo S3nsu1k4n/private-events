d1deed5d94720da72340b55ae55eb6fe8ee099ec
S3nsu1k4n <saschakoeppe@gmail.com>
2023-08-12 18:24:39 +0900

    add removable from attended event

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -6,7 +6,6 @@ class EventAttendeesController < ApplicationController
 
   def create
     @event_attendee = EventAttendee.new(event_attendee_id: current_user.id, attended_event_id: params[:event_id])
-
     if already_attends?
       flash[:notice] = 'Already attending'
       redirect_to root_path
@@ -17,6 +16,13 @@ class EventAttendeesController < ApplicationController
       flash[:notice] = "Can't attend this event"
       redirect_to root_path
     end
+  end
+
+  def destroy
+    @event_attendee = EventAttendee.find_by(event_attendee_id: current_user.id, attended_event_id: params[:event_id])
+    @event_attendee.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   def already_attends?
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :events, only: [:index, :new, :create] do
-    resources :event_attendees, only: [:new, :create]
+    resources :event_attendees, only: [:new, :create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
